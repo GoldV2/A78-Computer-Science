5cf8d87b9b49af48f7b896f9d7970abd8021b091
Rafael Almeida <rafaelpbcp@gmail.com>
2021-02-14 00:14:37 -0500

    Completed U4_M4_A1

--- U4/U4_M4/U4_M4_A1_RAlmeida/U4_M4_A1_RAlmeida.py
+++ U4/U4_M4/U4_M4_A1_RAlmeida/U4_M4_A1_RAlmeida.py
@@ -4,12 +4,12 @@
 
 from textwrap import dedent
 
-def step(n, name = ''):
+def task(n, name = ''):
     width = len(name) + 8
 
     s = f"""
          +{'-'*width}+
-         |{f'Step {n}':^{width}}|"""
+         |{f'Task {n}':^{width}}|"""
 
     if name:
         s += f"""
@@ -24,4 +24,35 @@ def step(n, name = ''):
 
     print(dedent(s))
 
-##
+task(1, 'Cities')
+
+cities_file = open('D:\\ralmeida\\Documents\\Classes\\A7-8 Computer Science Principles\\A78-Computer-Science-Classwork\\U4\\U4_M4\\U4_M4_A1_RAlmeida\\cities.txt', 'r')
+
+task(2)
+
+cities = cities_file.read()
+print(cities)
+
+task(3)
+
+pi_file = open('D:\\ralmeida\\Documents\\Classes\\A7-8 Computer Science Principles\\A78-Computer-Science-Classwork\\U4\\U4_M4\\U4_M4_A1_RAlmeida\\digits_of_pie.txt', 'r')
+
+pi_digits = ''
+for i in range(6):
+    pi_digits += pi_file.read(4)
+
+print(pi_digits)
+# subtracting one to remove the period
+print(len(pi_digits)-1)
+
+pi_file.close()
+
+task(4)
+
+initials = ''
+for char in cities:
+    if char.isupper() or char == '\n':
+        initials += char
+
+print(initials)
+cities_file.close()
