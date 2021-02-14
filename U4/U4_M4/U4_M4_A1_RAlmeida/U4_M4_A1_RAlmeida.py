4c48209948ddc7d0befc122e7a1fcff0ab74db72
Rafael Almeida <rafaelpbcp@gmail.com>
2021-02-14 00:17:59 -0500

    Completed U4_M4_A1

--- U4/U4_M4/U4_M4_A1_RAlmeida/U4_M4_A1_RAlmeida.py
+++ U4/U4_M4/U4_M4_A1_RAlmeida/U4_M4_A1_RAlmeida.py
@@ -26,33 +26,46 @@ def task(n, name = ''):
 
 task(1, 'Cities')
 
+# opening file
 cities_file = open('D:\\ralmeida\\Documents\\Classes\\A7-8 Computer Science Principles\\A78-Computer-Science-Classwork\\U4\\U4_M4\\U4_M4_A1_RAlmeida\\cities.txt', 'r')
 
 task(2)
 
+# reading the file as a string
 cities = cities_file.read()
 print(cities)
 
 task(3)
 
+# opening file
 pi_file = open('D:\\ralmeida\\Documents\\Classes\\A7-8 Computer Science Principles\\A78-Computer-Science-Classwork\\U4\\U4_M4\\U4_M4_A1_RAlmeida\\digits_of_pie.txt', 'r')
 
+# initizalizing an empty string
 pi_digits = ''
+# loop for 6 times
 for i in range(6):
+    # read 4 characters every iteration and add them to pi_digits
     pi_digits += pi_file.read(4)
 
+# printing pi_digits
 print(pi_digits)
-# subtracting one to remove the period
+# finding the amount of chars and subtracting one to remove the period
 print(len(pi_digits)-1)
 
 pi_file.close()
 
 task(4)
 
+# initializing empty string
 initials = ''
+# iterating through every character in cities
 for char in cities:
+    # checking if the character is uppercase or is a new line character
     if char.isupper() or char == '\n':
+        # if it is either add that to the empty string
         initials += char
 
+# print file
 print(initials)
+# close file
 cities_file.close()
